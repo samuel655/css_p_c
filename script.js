4a493345416172878acc78305ce3b02721d75851
samuel655 <luskmon6@gmail.com>
2021-09-23 00:48:16 -0500

    agregar curso a github

---
+++ script.js
@@ -0,0 +1,14 @@
+/*let midocumento = document.documentElement
+let contador = 10
+setInterval(() => {
+        contador++
+        if (contador > 18) {
+            contador = 10
+            midocumento.style.setProperty('--fontsize', '${contador}px')
+        } else {
+            midocumento.style.setProperty('--fontsize', '${contador}px')
+        }
+    }, 1000)
+    //midocumento.style.setProperty('color', 'black')*/
+/*let mystyles = window.getComputedStyle(document.querySelector('.section1'))
+console.log(mystyles)*/
